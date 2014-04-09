6596f1386659f86fe64df09d6131ca49f6586d25
Ralf Steuerwald <ralf.steuerwald@hs-hannover.de>
2014-04-09 16:13:02 +0200

    test: fix failing unit test

--- src/test/java/de/hshannover/f4/trust/ifmapj/identifier/ExtendedIdentifierTest.java
+++ src/test/java/de/hshannover/f4/trust/ifmapj/identifier/ExtendedIdentifierTest.java
@@ -7,17 +7,17 @@
  *    | | | |  | |_| \__ \ |_| | (_| |  _  |\__ \|  _  |
  *    |_| |_|   \__,_|___/\__|\ \__,_|_| |_||___/|_| |_|
  *                             \____/
- * 
+ *
  * =====================================================
- * 
+ *
  * Hochschule Hannover
  * (University of Applied Sciences and Arts, Hannover)
  * Faculty IV, Dept. of Computer Science
  * Ricklinger Stadtweg 118, 30459 Hannover, Germany
- * 
+ *
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de/
- * 
+ *
  * This file is part of ifmapj, version 1.0.1, implemented by the Trust@HsH
  * research group at the Hochschule Hannover.
  * %%
@@ -26,9 +26,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -105,7 +105,7 @@ public class ExtendedIdentifierTest {
 	public void testWithMultiAttr() throws ParserConfigurationException, MarshalException {
 		Document doc = dbf.newDocumentBuilder().newDocument();
 		String expected = "&lt;elname xmlns=&quot;urn://myuri&quot; ad=&quot;advalue&quot; "
-				+ "val=&quot;attr-val&quot;&gt; &lt;/elname&gt;";
+				+ "val=&quot;attr-val&quot;&gt;&lt;/elname&gt;";
 
 		Element el = doc.createElementNS("urn://myuri", "my:elname");
 		el.setAttributeNS(null, "val", "attr-val");
